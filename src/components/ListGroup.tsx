ab645c831be0b4b221b6152641277f577dfca9f0
Ahmed Hariedy <ahassan200695@gmail.com>
2024-09-30 21:34:59 +0300

    Added Conditional Functionality with Event Handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,25 +1,40 @@
+import { MouseEvent } from "react";
 
 function ListGroup ()
 {
 
-    const items = 
+    let items = 
     [
         'New York',
         'Port Said',
         'Texas',
     ];
 
-
+    const handleClick = (event : MouseEvent) => console.log(event);
     return ( 
   <>
+  <center>
     <h2>The List Group Component</h2>
+    {
+    items.length === 0 && <>
+    <div className="container w-75">
+      <div className="alert alert-danger" role="alert">
+  No Records To Show !
+</div>
+</div>
+</>
+}
       <ul className="list-group">
        {
-       items.map((item) => (
-       <li key={item} className="list-group-item">{item}</li>
+       items.map((item , index) => (
+       <li key={index} 
+       className="list-group-item"
+       onClick={handleClick}
+       >{item}</li>
        ))
        }
       </ul>
+      </center>
   </>
     );
 }
