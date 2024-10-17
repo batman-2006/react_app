d26e008f9fbe0cff3589ffb36d19e3e0e26f2b76
ahmed hariedy <113946682+batman-2006@users.noreply.github.com>
2024-10-18 00:08:35 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import { useState } from "react";
 
 function app ()
 {
-  const [Product , setProduct] = useState({
-   title: "T-shirt",
-   price: 1,
-  });
+
 
 return (<div>
   <h1>Hello React From Azure !</h1>
